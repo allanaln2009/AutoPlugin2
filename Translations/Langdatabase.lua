8ffd1b4db99b975c441531ebcbe0d54103435490
Állan Lima <allanaln2009@gmail.com>
2020-06-01 21:08:05 -0300

    Portuguese Brazil translation database translations file

--- Translations/Langdatabase.lua
+++ Translations/Langdatabase.lua
@@ -8,4 +8,5 @@ Online_Langs = {
 { id =  "GERMAN",		version = "01.07",		author =  "Schn1ek3" },
 { id =  "RUSSIAN",		version = "01.10",		author =  "Yoti" },
 { id =  "POLISH",		version = "01.06",		author =  "Gameplay World's" }
+{ id =  "PORTUGUESE_BR",		version = "01.00",		author =  "allanaln2009" }
 }
